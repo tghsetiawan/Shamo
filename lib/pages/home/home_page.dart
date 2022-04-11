56a96e9e631f15ac729bb41db9dbd30b7ca259fb
tghsetiawan <55044847+tghsetiawan@users.noreply.github.com>
2022-04-11 11:54:19 +0700

    13 Home page bagian header

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -6,11 +6,56 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'Home Page',
-        style: primaryTextStyle,
-      ),
+    Widget header() {
+      return Container(
+        margin: EdgeInsets.only(
+          top: defaultMargin,
+          left: defaultMargin,
+          right: defaultMargin,
+        ),
+        child: Row(
+          children: [
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Hallo, Teguh Setiawan',
+                    style: primaryTextStyle.copyWith(
+                      fontSize: 24,
+                      fontWeight: semiBold,
+                    ),
+                  ),
+                  Text(
+                    '@tgh_setiawan',
+                    style: subtitleTextStyle.copyWith(
+                      fontSize: 16,
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Container(
+              width: 54,
+              height: 54,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                image: DecorationImage(
+                  image: AssetImage(
+                    'assets/image_profile.png',
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      );
+    }
+
+    return ListView(
+      children: [
+        header(),
+      ],
     );
   }
 }
