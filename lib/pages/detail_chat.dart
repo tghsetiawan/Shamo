28c5f4fc7a979dd86cc18ff8cd7c52345fbca622
tghsetiawan <55044847+tghsetiawan@users.noreply.github.com>
2022-04-12 17:07:01 +0700

    #22 Detail chat page bagian chat bubble

--- lib/pages/detail_chat.dart
+++ lib/pages/detail_chat.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shamo/theme.dart';
+import 'package:shamo/widgets/chat_bubble.dart';
 
 class DetailChatPage extends StatelessWidget {
   const DetailChatPage({Key? key}) : super(key: key);
@@ -147,10 +148,29 @@ class DetailChatPage extends StatelessWidget {
       );
     }
 
+    Widget content() {
+      return ListView(
+        padding: EdgeInsets.symmetric(
+          horizontal: defaultMargin,
+        ),
+        children: [
+          ChatBubble(
+            isSender: true,
+            text: 'Hi, This item is still available?',
+          ),
+          ChatBubble(
+            isSender: false,
+            text: 'Good Night, This item is only available in size 42 and 43',
+          ),
+        ],
+      );
+    }
+
     return Scaffold(
       backgroundColor: backgroundColor3,
       appBar: header(),
       bottomNavigationBar: chatInput(),
+      body: content(),
     );
   }
 }
