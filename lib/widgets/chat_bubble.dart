8970d6b1f3e552a3df32912d2e386e5c6eb053e4
tghsetiawan <55044847+tghsetiawan@users.noreply.github.com>
2022-04-13 11:47:34 +0700

    #23 Detail chat page bagian chat bubble product preview

--- lib/widgets/chat_bubble.dart
+++ lib/widgets/chat_bubble.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:shamo/theme.dart';
 
 class ChatBubble extends StatelessWidget {
@@ -6,41 +7,149 @@ class ChatBubble extends StatelessWidget {
 
   final String text;
   final bool isSender;
+  final bool hasProduct;
 
-  ChatBubble({this.isSender = false, this.text = ''});
+  ChatBubble({
+    this.isSender = false,
+    this.text = '',
+    this.hasProduct = false,
+  });
 
   @override
   Widget build(BuildContext context) {
+    Widget productPreview() {
+      return Container(
+        width: 230,
+        margin: const EdgeInsets.only(bottom: 12),
+        padding: const EdgeInsets.all(12),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(isSender ? 12 : 0),
+            topRight: Radius.circular(isSender ? 0 : 12),
+            bottomRight: const Radius.circular(12),
+            bottomLeft: const Radius.circular(12),
+          ),
+          color: isSender ? backgroundColor5 : backgroundColor4,
+        ),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(12),
+                  child: Image.asset(
+                    'assets/image_shoes.png',
+                    width: 70,
+                  ),
+                ),
+                const SizedBox(
+                  width: 8,
+                ),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'COURT VISION 2.0 SHOES',
+                        style: primaryTextStyle,
+                      ),
+                      SizedBox(
+                        height: 4,
+                      ),
+                      Text(
+                        '\$57,15',
+                        style: priceTextStyle.copyWith(
+                          fontWeight: medium,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                OutlinedButton(
+                  onPressed: () {},
+                  style: OutlinedButton.styleFrom(
+                    side: BorderSide(
+                      color: primaryColor,
+                    ),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                  ),
+                  child: Text(
+                    'Add to Cart',
+                    style: purpleTextStyle,
+                  ),
+                ),
+                SizedBox(
+                  width: 8,
+                ),
+                TextButton(
+                  onPressed: () {},
+                  style: TextButton.styleFrom(
+                      backgroundColor: priceColor,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      )),
+                  child: Text(
+                    'Buy Now',
+                    style: GoogleFonts.poppins(
+                      color: backgroundColor5,
+                      fontWeight: medium,
+                    ),
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      );
+    }
+
     return Container(
       width: double.infinity,
       margin: const EdgeInsets.only(top: 30),
-      child: Row(
-        mainAxisAlignment:
-            isSender ? MainAxisAlignment.end : MainAxisAlignment.start,
+      child: Column(
+        crossAxisAlignment:
+            isSender ? CrossAxisAlignment.end : CrossAxisAlignment.start,
         children: [
-          Flexible(
-            child: Container(
-              constraints: BoxConstraints(
-                maxWidth: MediaQuery.of(context).size.width * 0.65,
-              ),
-              padding: const EdgeInsets.symmetric(
-                horizontal: 16,
-                vertical: 12,
-              ),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(isSender ? 12 : 0),
-                  topRight: Radius.circular(isSender ? 0 : 12),
-                  bottomRight: Radius.circular(12),
-                  bottomLeft: Radius.circular(12),
+          hasProduct ? productPreview() : SizedBox(),
+          Row(
+            mainAxisAlignment:
+                isSender ? MainAxisAlignment.end : MainAxisAlignment.start,
+            children: [
+              Flexible(
+                child: Container(
+                  constraints: BoxConstraints(
+                    maxWidth: MediaQuery.of(context).size.width * 0.6,
+                  ),
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 16,
+                    vertical: 12,
+                  ),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(isSender ? 12 : 0),
+                      topRight: Radius.circular(isSender ? 0 : 12),
+                      bottomRight: const Radius.circular(12),
+                      bottomLeft: const Radius.circular(12),
+                    ),
+                    color: isSender ? backgroundColor5 : backgroundColor4,
+                  ),
+                  child: Text(
+                    text,
+                    style: primaryTextStyle,
+                  ),
                 ),
-                color: isSender ? backgroundColor5 : backgroundColor4,
               ),
-              child: Text(
-                text,
-                style: primaryTextStyle,
-              ),
-            ),
+            ],
           ),
         ],
       ),
