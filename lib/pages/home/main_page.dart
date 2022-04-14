6ffb2a350537cd0f1a3bae13f804c3ebd0e12ce7
tghsetiawan <55044847+tghsetiawan@users.noreply.github.com>
2022-04-14 14:48:55 +0700

    #30 Mengubah warna background pada bottom navigation

--- lib/pages/home/main_page.dart
+++ lib/pages/home/main_page.dart
@@ -30,18 +30,18 @@ class _MainPageState extends State<MainPage> {
 
     Widget customBottomNav() {
       return ClipRRect(
-        borderRadius: BorderRadius.vertical(
+        borderRadius: const BorderRadius.vertical(
           top: Radius.circular(30),
         ),
         child: BottomAppBar(
-          shape: CircularNotchedRectangle(),
+          shape: const CircularNotchedRectangle(),
           notchMargin: 12,
           clipBehavior: Clip.antiAlias,
           child: BottomNavigationBar(
             backgroundColor: backgroundColor4,
             currentIndex: currentIndex,
             onTap: (value) {
-              print(value);
+              // print(value);
               setState(() {
                 currentIndex = value;
               });
@@ -50,56 +50,64 @@ class _MainPageState extends State<MainPage> {
             items: [
               BottomNavigationBarItem(
                 icon: Container(
-                  margin: EdgeInsets.only(
+                  margin: const EdgeInsets.only(
                     top: 20,
                     bottom: 10,
                   ),
                   child: Image.asset(
                     'assets/icon_home.png',
                     width: 21,
-                    color: currentIndex == 0 ? primaryColor : Color(0xff808191),
+                    color: currentIndex == 0
+                        ? primaryColor
+                        : const Color(0xff808191),
                   ),
                 ),
                 label: '',
               ),
               BottomNavigationBarItem(
                 icon: Container(
-                  margin: EdgeInsets.only(
+                  margin: const EdgeInsets.only(
                     top: 20,
                     bottom: 10,
                   ),
                   child: Image.asset(
                     'assets/icon_chat.png',
                     width: 20,
-                    color: currentIndex == 1 ? primaryColor : Color(0xff808191),
+                    color: currentIndex == 1
+                        ? primaryColor
+                        : const Color(0xff808191),
                   ),
                 ),
                 label: '',
               ),
               BottomNavigationBarItem(
                 icon: Container(
-                  margin: EdgeInsets.only(
+                  margin: const EdgeInsets.only(
                     top: 20,
                     bottom: 10,
                   ),
                   child: Image.asset(
                     'assets/icon_wishlist.png',
                     width: 20,
-                    color: currentIndex == 2 ? primaryColor : Color(0xff808191),
+                    color: currentIndex == 2
+                        ? primaryColor
+                        : const Color(0xff808191),
                   ),
                 ),
                 label: '',
               ),
               BottomNavigationBarItem(
                 icon: Container(
-                  margin: EdgeInsets.only(
+                  margin: const EdgeInsets.only(
                     top: 20,
                     bottom: 10,
                   ),
                   child: Image.asset(
                     'assets/icon_profile.png',
                     width: 18,
-                    color: currentIndex == 3 ? primaryColor : Color(0xff808191),
+                    color: currentIndex == 3
+                        ? primaryColor
+                        : const Color(0xff808191),
                   ),
                 ),
                 label: '',
@@ -113,24 +121,24 @@ class _MainPageState extends State<MainPage> {
     Widget body() {
       switch (currentIndex) {
         case 0:
-          return HomePage();
+          return const HomePage();
           break;
         case 1:
-          return ChatPage();
+          return const ChatPage();
           break;
         case 2:
-          return WishlistPage();
+          return const WishlistPage();
           break;
         case 3:
-          return ProfilePage();
+          return const ProfilePage();
           break;
         default:
-          return HomePage();
+          return const HomePage();
       }
     }
 
     return Scaffold(
-      backgroundColor: backgroundColor1,
+      backgroundColor: currentIndex == 0 ? backgroundColor1 : backgroundColor3,
       floatingActionButton: cartButton(),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       bottomNavigationBar: customBottomNav(),
