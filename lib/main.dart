e3eb2b0e762460d00efef88b221a66cb997ec479
tghsetiawan <55044847+tghsetiawan@users.noreply.github.com>
2022-04-14 15:05:21 +0700

    #31 Product page bagian header navigasi

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:shamo/pages/detail_chat.dart';
 import 'package:shamo/pages/edit_profile_page.dart';
 import 'package:shamo/pages/home/main_page.dart';
+import 'package:shamo/pages/product_page.dart';
 import 'package:shamo/pages/sign_in_page.dart';
 import 'package:shamo/pages/sign_up_page.dart';
 import 'package:shamo/pages/splash_page.dart';
@@ -17,12 +18,13 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       initialRoute: '/',
       routes: {
-        '/': (context) => SplashPage(),
-        '/sign-in': (context) => SignInPage(),
-        '/sign-up': (context) => SignUpPage(),
-        '/home': (context) => MainPage(),
-        '/detail-chat': (context) => DetailChatPage(),
-        '/edit-profile': (context) => EditProfilePage(),
+        '/': (context) => const SplashPage(),
+        '/sign-in': (context) => const SignInPage(),
+        '/sign-up': (context) => const SignUpPage(),
+        '/home': (context) => const MainPage(),
+        '/detail-chat': (context) => const DetailChatPage(),
+        '/edit-profile': (context) => const EditProfilePage(),
+        '/product': (context) => const ProductPage(),
       },
     );
   }
--- lib/theme.dart
+++ lib/theme.dart
@@ -12,6 +12,7 @@ Color backgroundColor2 = Color(0xff2b2937);
 Color backgroundColor3 = Color(0xff242231);
 Color backgroundColor4 = Color(0xff252836);
 Color backgroundColor5 = Color(0xff2b2844);
+Color backgroundColor6 = Color(0xffecedef);
 Color primaryTextColor = Color(0xfff1f0f2);
 Color secondaryTextColor = Color(0xff999999);
 Color subtitleColor = Color(0xff504f5e);
