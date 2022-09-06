4b212ac56d5324a9d7c9093f4252a56080290295
Teguh <teguhs.sgr@gmail.com>
2022-09-06 15:10:16 +0700

    #41 Checkout Page Bagian Payment Summary

--- lib/pages/checkout_page.dart
+++ lib/pages/checkout_page.dart
@@ -130,6 +130,120 @@ class CheckoutPage extends StatelessWidget {
             ],
           ),
         ),
+
+        // NOTE : Payment Summary
+        Container(
+          margin: EdgeInsets.only(
+            top: defaultMargin,
+          ),
+          padding: const EdgeInsets.all(20),
+          decoration: BoxDecoration(
+            color: backgroundColor4,
+            borderRadius: BorderRadius.circular(12),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Payment Summary',
+                style: primaryTextStyle.copyWith(
+                  fontWeight: medium,
+                  fontSize: 16,
+                ),
+              ),
+              const SizedBox(
+                height: 12,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Product Quantity',
+                    style: secondaryTextStyle.copyWith(
+                      fontWeight: light,
+                      fontSize: 12,
+                    ),
+                  ),
+                  Text(
+                    '2 Items',
+                    style: primaryTextStyle.copyWith(
+                      fontWeight: medium,
+                    ),
+                  )
+                ],
+              ),
+              const SizedBox(
+                height: 12,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Product Price',
+                    style: secondaryTextStyle.copyWith(
+                      fontWeight: light,
+                      fontSize: 12,
+                    ),
+                  ),
+                  Text(
+                    '\$575.96',
+                    style: primaryTextStyle.copyWith(
+                      fontWeight: medium,
+                    ),
+                  )
+                ],
+              ),
+              const SizedBox(
+                height: 12,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Shipping',
+                    style: secondaryTextStyle.copyWith(
+                      fontWeight: light,
+                      fontSize: 12,
+                    ),
+                  ),
+                  Text(
+                    'Free',
+                    style: primaryTextStyle.copyWith(
+                      fontWeight: medium,
+                    ),
+                  )
+                ],
+              ),
+              const SizedBox(
+                height: 12,
+              ),
+              const Divider(
+                thickness: 1,
+                color: Color(0xff2e3141),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Total',
+                    style: priceTextStyle.copyWith(
+                      fontWeight: semiBold,
+                    ),
+                  ),
+                  Text(
+                    '\$575.96',
+                    style: priceTextStyle.copyWith(
+                      fontWeight: semiBold,
+                    ),
+                  )
+                ],
+              ),
+            ],
+          ),
+        )
       ],
     );
   }
